c801ed0f9de3986f3e8ac1fcca885917159c2937
Hsiang-Lin Chan <82668977+Hsiang-LinC@users.noreply.github.com>
2026-08-29 14:48:13 +0200

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        '''
+            binary search for O(logn) time complexity
+        '''
+        lp, rp = 0, len(nums) - 1
+        while lp <= rp:
+            mid = (lp + rp) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lp = mid + 1
+            else:
+                rp = mid - 1
+        return -1
